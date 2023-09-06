af1be2169a444f9a367232c002ba5c2c72c20072
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-06 09:22:12 -0300

    Creating the Studio Domain

---
+++ senai.inlock.webApi/Domains/StudioDomain.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai.inlock.webApi.Domains
+{
+    /// <summary>
+    /// This class is for the representation of studio in database
+    /// </summary>
+    public class StudioDomain
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do estúdio é obrigatório")]
+        public int Name { get; set; }
+    }
+}
