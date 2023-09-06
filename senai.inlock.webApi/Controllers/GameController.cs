9dca6d8ec177a45bd5518d9cb986ff66004b35e1
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-06 10:43:49 -0300

    Creating the feature of list all games

---
+++ senai.inlock.webApi/Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai.inlock.webApi.Domains;
+using senai.inlock.webApi.Interfaces;
+using senai.inlock.webApi.Repositories;
+
+namespace senai.inlock.webApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class GameController : ControllerBase
+    {
+        private IGameRepository gameRepository;
+
+        public GameController()
+        {
+            gameRepository = new GameRepository();
+        }
+
+        [HttpGet]
+        public IActionResult FindAll()
+        {
+            try
+            {
+                List<GameDomain> games = gameRepository.FindAll();
+
+                return Ok(games);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+    }
+}
---
+++ senai.inlock.webApi/Repositories/GameRepository.cs
@@ -0,0 +1,45 @@
+﻿using senai.inlock.webApi.Domains;
+using senai.inlock.webApi.Interfaces;
+using System.Data.SqlClient;
+
+namespace senai.inlock.webApi.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        private string connectionString = "Data Source = NOTE17-S14; Initial Catalog = inlock_games; User Id = sa; Pwd = Senai@134";
+
+        public List<GameDomain> FindAll()
+        {
+            List<GameDomain> games = new List<GameDomain>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * From Game INNER JOIN Studio ON Game.IdStudio = Studio.Id;";
+
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        GameDomain game = new GameDomain
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            IdStudio = Convert.ToInt32(reader["IdStudio"]),
+                            Name = reader["Name"].ToString(),
+                            Description = reader["Description"].ToString(),
+                            ReleaseDate = DateTime.Parse(reader["ReleaseDate"].ToString()),
+                            Price = float.Parse(reader["Price"].ToString())
+                        };
+
+                        games.Add(game);
+                    }
+                }
+            }
+
+            return games;
+        }
+    }
+}
