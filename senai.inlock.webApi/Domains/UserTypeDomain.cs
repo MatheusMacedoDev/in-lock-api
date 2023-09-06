cc431123e0cb9ce4140c75fb376018f1fea53b96
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-06 09:55:12 -0300

    Creating the UserType Domain

---
+++ senai.inlock.webApi/Domains/UserTypeDomain.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai.inlock.webApi.Domains
+{
+    /// <summary>
+    /// This class is for the representation of user type in database
+    /// </summary>
+    public class UserTypeDomain
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do tipo de usuário é obrigatório!")]
+        public string TypeName { get; set; }
+    }
+}
