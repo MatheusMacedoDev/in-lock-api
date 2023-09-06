c380ac3bc9ebd4c12d621af428e6e1be0693326d
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-06 09:27:55 -0300

    Creating the Game Domain

---
+++ senai.inlock.webApi/Domains/GameDomain.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai.inlock.webApi.Domains
+{
+    /// <summary>
+    /// This class is for the representation of game in database
+    /// </summary>
+    public class GameDomain
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O id do estúdio é obrigatório")]
+        public int IdStudio { get; set; }
+
+        [Required(ErrorMessage = "O nome do jogo é obrigatório")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get;  set; }
+        public float Price { get; set; }
+    }
+}
