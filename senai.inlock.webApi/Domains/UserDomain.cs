0b56ea24e470163f08fff6f05d0bb254e9849243
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-06 09:58:43 -0300

    Creating the User Domain

---
+++ senai.inlock.webApi/Domains/UserDomain.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai.inlock.webApi.Domains
+{
+    /// <summary>
+    /// This class is for the representation of user in database
+    /// </summary>
+    public class UserDomain
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O usuário deve possuir um tipo de usuário !")]
+        public int IdUserType { get; set; }
+
+        [Required(ErrorMessage = "O usuário deve possuir e-mail!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O usuário deve possuir senha!")]
+        public string Password { get; set; }
+    }
+}
